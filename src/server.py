2d95117fc48f7a6d866798ba20dd9131bf13b7ea
anders-fisk <C4035412@newcastle.ac.uk>
2026-08-02 22:58:41 +0100

    Change port that server is run on to default SSH port

--- src/server.py
+++ src/server.py
@@ -170,7 +170,7 @@ def listen():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("", 2222))
+    sock.bind(("", 22))
     sock.listen(100)
     client, addr = sock.accept()
     t = paramiko.Transport(client)
